7b3b8d455311b95db88516fb3f82bc4d2a202d93
Sekou360 <camaravibescooll@gmail.com>
2026-09-01 05:48:13 +0200

    Refactor session variables in Configurations.js

--- Configurations.js
+++ Configurations.js
@@ -2,10 +2,12 @@ import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 
 global.mongodb = process.env.MONGO_URL || "mongodb+srv://camaravibescooll_db_user:Sekou123456@cluster0.ptfaaub.mongodb.net/atlas-md?retryWrites=true&w=majority";
-global.sessionName = process.env.SESSION_ID || "SekouAtlas2026";
+global.sessionId = process.env.SESSION_ID || "SekouAtlas2026";
+global.sessionName = global.sessionId;
 
 export default {
   mongodb: global.mongodb,
+  sessionId: global.sessionId,
   sessionName: global.sessionName,
   PREFIX: "."
 };
